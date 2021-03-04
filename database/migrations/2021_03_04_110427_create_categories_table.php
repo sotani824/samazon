bc3ffd6800194b2f1ffd33292c11f15421694981
sotani824 <sotani824@gmail.com>
2021-03-04 21:01:03 +0900

    create Category model migrate

--- database/migrations/2021_03_04_110427_create_categories_table.php
+++ database/migrations/2021_03_04_110427_create_categories_table.php
@@ -16,8 +16,8 @@ class CreateCategoriesTable extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
-            $table->test('description');
-            $table->string('major_category_name')
+            $table->text('description');
+            $table->string('major_category_name');
             $table->timestamps();
         });
     }
