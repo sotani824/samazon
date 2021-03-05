05790c824bfdbfb5268d5317cf5c6ece4f9b1171
sotani824 <sotani824@gmail.com>
2021-03-05 21:03:45 +0900

    profit

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -21,7 +21,11 @@ class CartController extends Controller
         $total = 0;
 
         foreach ($cart as $c) {
-            $total += $c->qty * $c->price;
+            if($c->options->carriage) {
+                $total += ($c->qty * ($c->price + 800));
+            } else {
+                $total += $c->qty * $c->price;
+            }
         }
 
         return view('carts.index', compact('cart', 'total'));
@@ -37,11 +41,14 @@ class CartController extends Controller
     {
         Cart::instance(Auth::user()->id)->add(
             [
-                'id' => $request->_token, 
-                'name' => $request->name, 
-                'qty' => $request->qty, 
-                'price' => $request->price, 
+                'id' => $request->_token,
+                'name' => $request->name,
+                'qty' => $request->qty,
+                'price' => $request->price,
                 'weight' => $request->weight,
+                'options' => [
+                    'carriage' => $request->carriage
+                ]
             ]
         );
 
@@ -87,14 +94,41 @@ class CartController extends Controller
      */
     public function destroy(Request $request)
     {
-        $user_shoppingcarts = DB::table('shoppingcart')->where('instance', Auth::user()->id)->get();
-
+        $user_shoppingcarts = DB::table('shoppingcart')->get();
+        $number = DB::table('shoppingcart')->where('instance', Auth::user()->id)->count();
+ 
         $count = $user_shoppingcarts->count();
 
         $count += 1;
-        Cart::instance(Auth::user()->id)->store;
+        $number += 1;
+        $cart = Cart::instance(Auth::user()->id)->content();
+ 
+        $price_total = 0;
+        $qty_total = 0;
 
-        DB::table('shoppingcart')->where('instance', Auth::user()->id)->where('number', null)->update(['number' => $count, 'buy_flag' => true]);
+        foreach ($cart as $c) {
+            if ($c->options->carriage) {
+                $price_total += ($c->qty * ($c->price + 800));
+            } else {
+                $price_total += $c->qty * $c->price;
+            }
+            $qty_total += $c->qty;
+        }
+
+        Cart::instance(Auth::user()->id)->store($count);
+
+        DB::table('shoppingcart')->where('instance', Auth::user()->id)
+                                 ->where('number', null)
+                                 ->update(
+                                    [
+                                        'code' => substr(str_shuffle('1234567890abcdefghijklmnopqrstuvwxyz'), 0, 10),
+                                        'number' => $number, 
+                                        'price_total' => $price_total,
+                                        'qty' => $qty_total,
+                                        'buy_flag' => true, 
+                                        'updated_at' => date("Y/m/d H:i:s")
+                                    ]
+                                 );
 
         Cart::instance(Auth::user()->id)->destroy();
 
