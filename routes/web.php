29ac3e29702d69b17512e05e90bf072886a9dc3a
sotani824 <sotani824@gmail.com>
2021-03-05 17:36:38 +0900

    dashboard toppage

--- routes/web.php
+++ routes/web.php
@@ -32,3 +32,9 @@ Route::resource('products', 'ProductController');
 Auth::routes(['verify' => true]);
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/dashboard', 'DashboardController@index');
+
+if (env('APP_ENV') === 'production') {
+    URL::forceScheme('https');
+}
