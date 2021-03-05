b8a17a6703dea57af94929aa2d4a51fdf75c685a
sotani824 <sotani824@gmail.com>
2021-03-05 15:30:13 +0900

    create mypage

--- app/User.php
+++ app/User.php
@@ -28,7 +28,7 @@ class User extends Authenticatable implements MustVerifyEmail
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 'postal_code', 'address', 'phone'
     ];
 
     /**
