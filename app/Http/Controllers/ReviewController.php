af1f554ad95e87bec3114f5fe6a2ea6776352875
sotani824 <sotani824@gmail.com>
2021-03-05 14:38:04 +0900

    create ReviewController

---
+++ app/Http/Controllers/ReviewController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Review;
+use Illuminate\Http\Request;
+
+class ReviewController extends Controller
+{
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Product $product, Request $request)
+    {
+        $review = new Review();
+        $review->content = $request->input('content');
+        $review->product_id = $product->id;
+        $review->user_id = Auth::user()->id;
+        $review->save();
+
+        return redirect()->route('products.show', $product);
+    }
+}
