13ab0260427dd24a513daa3311ebe9c8c1f79b57
sotani824 <sotani824@gmail.com>
2021-03-05 20:36:45 +0900

    user destroy

--- routes/web.php
+++ routes/web.php
@@ -12,11 +12,12 @@
 */
 
 Route::get('/', 'WebController@index');
-
+  
 Route::get('users/carts', 'CartController@index')->name('carts.index');
 Route::post('users/carts', 'CartController@store')->name('carts.store');
+Route::put('users/carts', 'CartController@update')->name('carts.update');
 Route::delete('users/carts', 'CartController@destroy')->name('carts.destroy');
-
+  
 Route::get('users/mypage', 'UserController@mypage')->name('mypage');
 Route::get('users/mypage/edit', 'UserController@edit')->name('mypage.edit');
 Route::get('users/mypage/address/edit', 'UserController@edit_address')->name('mypage.edit_address');
@@ -24,6 +25,7 @@ Route::put('users/mypage', 'UserController@update')->name('mypage.update');
 Route::get('users/mypage/favorite', 'UserController@favorite')->name('mypage.favorite');
 Route::get('users/mypage/password/edit', 'UserController@edit_password')->name('mypage.edit_password');
 Route::put('users/mypage/password', 'UserController@update_password')->name('mypage.update_password');
+Route::delete('users/mypage/delete', 'UserController@destroy')->name('mypage.destroy');
 
 Route::post('products/{product}/reviews', 'ReviewController@store');
 
@@ -34,15 +36,13 @@ Auth::routes(['verify' => true]);
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/dashboard', 'DashboardController@index')->middleware('auth:admins');
+Route::get('/dashboard', 'DashboardController@index');
 
-Route::group(['prefix' => 'dashboard', 'as' => 'dashboard.'], function () {
-    Route::get('login', 'Dashboard\Auth\LoginController@showLoginForm')->name('login');
-    Route::post('login', 'Dashboard\Auth\LoginController@login')->name('login');
-    Route::resource('major_categories', 'Dashboard\MajorCategoryController')->middleware('auth:admins');
-    Route::resource('categories', 'Dashboard\CategoryController')->middleware('auth:admins');
-    Route::resource('products', 'Dashboard\ProductController')->middleware('auth:admins');
-    Route::resource('users', 'Dashboard\UserController')->middleware('auth:admins');
+Route::group(['prefix' => 'dashboard', 'as' => 'dashboard.'], function() {
+    Route::resource('major_categories', 'Dashboard\MajorCategoryController');
+    Route::resource('categories', 'Dashboard\CategoryController');
+    Route::resource('products', 'Dashboard\ProductController');
+    Route::resource('users', 'Dashboard\UserController');
 });
 
 if (env('APP_ENV') === 'production') {
