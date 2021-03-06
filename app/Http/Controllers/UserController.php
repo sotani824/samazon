4b1f714554a9ded7e990336eb28ae3da6ec6fcbf
sotani824 <sotani824@gmail.com>
2021-03-06 10:53:50 +0900

    cart history

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers;
 
 use App\User;
 use App\Product;
+use App\ShoppingCart;
+use Gloudemans\Shoppingcart\Facades\Cart;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
@@ -80,24 +84,118 @@ class UserController extends Controller
     {
         $user = Auth::user();
 
-        $favorites = $user->favorites(Product::class)->get();
+        $favorites = $user->favorites()->get();
 
         return view('users.favorite', compact('favorites'));
     }
+
     public function destroy(Request $request)
     {
         $user = Auth::user();
-         
+        
         if ($user->deleted_flag) {
             $user->deleted_flag = false;
         } else {
             $user->deleted_flag = true;
         }
- 
+
         $user->update();
- 
+
         Auth::logout();
- 
+
         return redirect('/');
+    }
+
+    public function register_card(Request $request)
+    {
+        $user = Auth::user();
+
+        $pay_jp_secret = env('PAYJP_SECRET_KEY');
+        \Payjp\Payjp::setApiKey($pay_jp_secret);
+
+        $card = [];
+        $count = 0;
+
+        if ($user->token != "") {
+            $result = \Payjp\Customer::retrieve($user->token)->cards->all(array("limit"=>1))->data[0];
+            $count = \Payjp\Customer::retrieve($user->token)->cards->all()->count;
+
+            $card = [
+                'brand' => $result["brand"],
+                'exp_month' => $result["exp_month"],
+                'exp_year' => $result["exp_year"],
+                'last4' => $result["last4"] 
+            ];
+        }
+
+        return view('users.register_card', compact('card', 'count'));
+    }
+
+    public function token(Request $request)
+    {
+        $pay_jp_secret = env('PAYJP_SECRET_KEY');
+        \Payjp\Payjp::setApiKey($pay_jp_secret);
+
+        $user = Auth::user();
+        $customer = $user->token;
+
+        if ($customer != "") {
+            $cu = \Payjp\Customer::retrieve($customer);
+            $delete_card = $cu->cards->retrieve($cu->cards->data[0]["id"]);
+            $delete_card->delete();
+            $cu->cards->create(array(
+                "card" => request('payjp-token')
+            ));
+        } else {
+            $cu = \Payjp\Customer::create(array(
+                "card" => request('payjp-token')
+            ));
+            $user->token = $cu->id;
+            $user->update();
+        }
+
+        return redirect()->route('mypage');
+    }
+
+    public function cart_history_index(Request $request)
+    {
+        $page = $request->page != null ? $request->page : 1;
+        $user_id = Auth::user()->id;
+        $billings = ShoppingCart::getCurrentUserOrders($user_id);
+        $total = count($billings);
+        $paginator = new LengthAwarePaginator(array_slice($billings, ($page - 1), 15), $total, 15, $page, ['path' => 'dashboard']);
+
+        return view('users.cart_history_index', compact('billings', 'total', 'paginator'));
+    }
+
+    public function cart_history_show(Request $request)
+    {
+        $num = $request->num;
+        $user_id = Auth::user()->id;
+
+        $cart_info = DB::table('shoppingcart')->where('instance', $user_id)->where('number', $num)->get()->first();
+
+        Cart::instance($user_id)->restore($num);
+
+        $cart_contents = Cart::content();
+
+        Cart::instance($user_id)->store($num);
+
+        Cart::destroy();
+
+        DB::table('shoppingcart')->where('instance', $user_id)
+                                 ->where('number', null)
+                                 ->update(
+                                     [
+                                         'code' => $cart_info->code,
+                                         'number' => $num, 
+                                         'price_total' => $cart_info->price_total,
+                                         'qty' => $cart_info->qty,
+                                         'buy_flag' => $cart_info->buy_flag, 
+                                         'updated_at' => $cart_info->updated_at
+                                     ]
+                                 );
+
+        return view('users.cart_history_show', compact('cart_contents', 'cart_info'));
     }
 }
