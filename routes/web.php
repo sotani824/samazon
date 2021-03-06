04dbf651f381c40ed2d85acd3ddc33ec91a46860
sotani824 <sotani824@gmail.com>
2021-03-06 10:44:00 +0900

    csv

--- routes/web.php
+++ routes/web.php
@@ -45,6 +45,8 @@ Route::group(['prefix' => 'dashboard', 'as' => 'dashboard.', 'middleware' => 'au
     Route::resource('major_categories', 'Dashboard\MajorCategoryController')->middleware('auth:admins');
     Route::resource('users', 'Dashboard\UserController')->middleware('auth:admins');
     Route::get('orders', 'Dashboard\OrderController@index')->middleware('auth:admins');
+    Route::get('products/import/csv', 'Dashboard\ProductController@import')->middleware('auth:admins');
+    Route::post('products/import/csv', 'Dashboard\ProductController@import_csv')->middleware('auth:admins');
 });
 
 if (env('APP_ENV') === 'production') {
